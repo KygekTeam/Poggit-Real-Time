96a2419c2e33bcf3d87338ab75b65fbd0605e2d9
KygekDev <68368066+Kygekraqmak@users.noreply.github.com>
2020-11-06 17:36:29 +0800

    Update realtime.php

--- realtime.php
+++ realtime.php
@@ -30,9 +30,12 @@
                 <hr />
                 <h2>Total Plugins Downloads (By Kygekraqmak and aminozomty):</h2>
                 <h1 id="kygekraqmak" style="font-size: 72px;"></h1>
+                <h3>Download our plugins <a href="https://poggit.pmmp.io/plugins/by/Kygekraqmak">here</a> and see the counter goes up!</h3>
                 <hr />
-                <h3 style="text-align: left;">This page automatically refreshes every 10 seconds.</h3>
-                <h3 style="text-align: left;">If you have any feedbacks or you want to contribute to make this site better, contact KygekDev#6415 on Discord.</h3>
+                <h4 style="text-align: left;">This page automatically refreshes every 5 seconds.</h4>
+                <h4 style="text-align: left;">If you have any feedbacks or you want to contribute to make this site better, contact KygekDev#6415 on Discord.</h4>
+                <hr />
+                <h4 style="text-align: left;"><a href="https://github.com/Kygekraqmak/Poggit-Real-Time/tree/refresh-h1">Source Code</a></h4>
                 <br>
             </div>
         </div>
