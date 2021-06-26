3df3f55c3eb96b90d4cd3cd9ab53ffb781505df9
KygekDev <68368066+KygekDev@users.noreply.github.com>
2021-06-26 14:41:57 +0800

    Added KygekFarmlandDecay plugin

--- assets/realtime.php
+++ assets/realtime.php
@@ -31,6 +31,7 @@ function getKygekDownloads(){
             "KygekWhitelistKick",
             "KygekTagsShop",
             "KygekEatHeal",
+            "KygekFarmlandDecay",
             "PerWorldPlayersList"
         ]);
     });
