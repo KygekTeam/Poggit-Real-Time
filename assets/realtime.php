4cacaed73cfb575877184521463018d62b01249d
KygekDev <68368066+KygekDev@users.noreply.github.com>
2021-06-26 14:44:44 +0800

    Update realtime.php

--- assets/realtime.php
+++ assets/realtime.php
@@ -1,29 +1,10 @@
 <?php
-function get_json($json) {
-    return json_decode(file_get_contents($json));
-}
-function getAllDownloads(){
-    $json = [];
-    array_push(
-        $json,
-        get_json('https://poggit.pmmp.io/releases.json?fields=downloads')
-    );
-    
-    $downloads = 0;
-    
-    foreach ($json as $jsonc) {
-        foreach ($jsonc as $jsoncc) {
-            $jsoncc = (array) $jsoncc;
-            $downloads += $jsoncc["downloads"];
-        }
-    }
-    return $downloads;
-}
-function getKygekDownloads(){
+
+function getDownloads(bool $kygekteam = false){
     $json = json_decode(file_get_contents("https://poggit.pmmp.io/plugins.json?fields=downloads,name"), true);
 
-    $json = array_filter($json, function ($var) : bool {
-        return in_array($var["name"], [
+    $json = array_filter($json, function ($var) use ($kygekteam) : bool {
+        if ($kygekteam && !in_array($var["name"], [
             "KygekJoinUI",
             "KygekRulesUI",
             "KygekRanksUI",
@@ -33,21 +14,32 @@ function getKygekDownloads(){
             "KygekEatHeal",
             "KygekFarmlandDecay",
             "PerWorldPlayersList"
-        ]);
+        ])) return false;
+
+        static $downloads = 0;
+        static $name = "";
+        if ($var["downloads"] !== $downloads || $var["name"] !== $name) {
+            $downloads = $var["downloads"];
+            $name = $var["name"];
+            return true;
+        }
+
+        return false;
     });
 
     $downloads = 0;
-    
     foreach ($json as $jsonc) {
         $downloads += $jsonc["downloads"];
     }
-    
+
     // KygekRulesUI version 1.1.0 downloads
     // Should be added because it's Kygekraqmak's plugin and the version is currently unavailable
-    $downloads += 957;
-    
+    if ($kygekteam) $downloads += 957;
+
     return $downloads;
 }
+
 ?>
-document.getElementById('poggit').innerHTML = <?php echo getAllDownloads(); ?>;
-document.getElementById('kygekraqmak').innerHTML = <?php echo getKygekDownloads(); ?>;
+
+document.getElementById('poggit').innerHTML = <?php echo getDownloads(); ?>;
+document.getElementById('kygekraqmak').innerHTML = <?php echo getDownloads(true); ?>;
